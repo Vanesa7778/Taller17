11c65d55585bc49dc02bf5047276e1a27856b931
Vanesa7778 <vanesaladinogomez@gmail.com>
2023-05-17 11:44:28 -0500

    vanesa

---
+++ tupla.py
@@ -0,0 +1,10 @@
+'''tipoColores =("naranaja","rojo","verde","amarillo")
+print(tipoColores[3])
+
+listaAnimales = ("perro", "gatos", "tortugas", "conejos")
+
+print(listaAnimales[1])
+
+
+tipoNombres=("melissa","kamila","vanessa","paula","stiven")
+print(tipoNombres[4])'''
